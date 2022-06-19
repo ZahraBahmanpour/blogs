01ce494052514d5357c774557f4bf17bc28e3038
zahrabahmanpour <z.bmnpr@gmail.com>
2022-06-19 10:34:19 +0430

    add BlogList component

---
+++ src/BlogList.js
@@ -0,0 +1,17 @@
+const BlogList = (props) => {
+  const { blogs, title } = props;
+  console.log(props);
+  return (
+    <div className="blogs-list">
+      <h2>{title}</h2>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>Written by {blog.author}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default BlogList;
