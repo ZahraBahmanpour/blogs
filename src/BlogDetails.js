1d7a9f1ae576e48a4a92982e4047d37848ede265
zahrabahmanpour <z.bmnpr@gmail.com>
2022-06-20 12:31:57 +0430

    add delete blog

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -1,13 +1,23 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useHistory } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
   const { id } = useParams();
+  const history = useHistory();
+
   const {
     data: blog,
     isLoading,
     error,
   } = useFetch('http://localhost:8000/blogs/' + id);
+
+  const handleDelete = () => {
+    fetch('http://localhost:8000/blogs/' + blog.id, { method: 'DELETE' }).then(
+      () => {
+        history.push('/');
+      }
+    );
+  };
   return (
     <div className="blog-details">
       {isLoading && <div>Loading...</div>}
@@ -17,6 +27,7 @@ const BlogDetails = () => {
           <h2>{blog.title}</h2>
           <p>Written by {blog.author}</p>
           <div>{blog.body}</div>
+          <button onClick={handleDelete}>Delete</button>
         </article>
       )}
     </div>
