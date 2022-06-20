b30c90bc16af2c09b2208998465480866487fa10
zahrabahmanpour <z.bmnpr@gmail.com>
2022-06-20 12:22:53 +0430

    add create page and post request

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,47 @@
+import { useState } from 'react';
 const Create = () => {
+  const [title, setTitle] = useState('');
+  const [body, setBody] = useState('');
+  const [author, setAuthor] = useState('mario');
+  const [isLoading, setIsLoading] = useState(false);
+
+  const handlerSubmit = (e) => {
+    e.preventDefault();
+    const blog = { title, body, author };
+    fetch('http://localhost:8000/blogs', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify(blog),
+    }).then(() => {
+      console.log('new blog added');
+      setIsLoading(false);
+    });
+  };
   return (
     <div className="create">
       <h2>Add a New Blog</h2>
+      <form onSubmit={handlerSubmit}>
+        <label>Blog Title</label>
+        <input
+          type="text"
+          required
+          value={title}
+          onChange={(e) => setTitle(e.target.value)}
+        />
+        <label>Blog Body</label>
+        <textarea
+          required
+          value={body}
+          onChange={(e) => setBody(e.target.value)}
+        />
+        <label>Blog Author</label>
+        <select value={author} onChange={(e) => setAuthor(e.target.value)}>
+          <option value="mario">mario</option>
+          <option value="yoshi">yoshi</option>
+        </select>
+        {!isLoading && <button>Add Blog</button>}
+        {isLoading && <button disabled>Adding Blog...</button>}
+      </form>
     </div>
   );
 };
